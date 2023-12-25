ef6d5644657ca504dcc075515b07946c4919ffc9
osadavc <osadavidath@gmail.com>
2023-12-25 22:34:38 +0530

    🍱 Updated favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>TextQuest</title>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
