369dafb878721211515e26db575fefaceb663e70
osadavc <osadavidath@gmail.com>
2023-12-26 22:25:48 +0530

    🗃️ Configured prisma

---
+++ src/components/Home/HeroSection.tsx
@@ -0,0 +1,5 @@
+const HeroSection = () => {
+  return <div></div>;
+};
+
+export default HeroSection;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Common/Header";
+import HeroSection from "@/components/Home/HeroSection";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 };
